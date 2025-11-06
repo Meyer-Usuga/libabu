4ea190fc31d18f7c0ddd5dd611555b0d258f5150
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2025-11-06 00:08:26 -0500

    changes: se actualiza el componente de layout para integrar el sidebar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@layout';
+import { SidebarComponent } from './sidebar';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
