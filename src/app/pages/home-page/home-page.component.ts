07e54e4340191eae1fcfbc718a6ff330153aac69
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2025-11-13 22:31:03 -0500

    feat: se añade la pagina de inicio

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ButtonComponent } from 'app/shared/components';
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ButtonComponent],
   standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
